73dc6a4b5c0109bba79bc19570fcfd40dd094b40
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2022-01-08 22:35:24 +0530

    product card component

--- frontend/src/component/Home/Home.jsx
+++ frontend/src/component/Home/Home.jsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import { CgMouse } from "react-icons/all";
 import "./Home.css";
+import Product from './Product';
+
+const product = {
+    name: "shirt",
+    images: [{ url: "https://i.ibb.co/DRST11n/1.webp" }],
+    price: "$300",
+    _id: "neha",
+}
 
 const Home = () => {
     return (
@@ -17,6 +25,9 @@ const Home = () => {
             </div>
 
             <h2 className="homeHeading">Featured Products</h2>
+            <div className='container' id="container">
+                <Product product={product} />
+            </div>
         </>
     )
 }
