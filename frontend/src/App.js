bf4f70cf41796f54beedf61ec636a434fc64e998
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2022-01-13 13:39:48 +0530

    product details + review card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import WebFont from "webfontloader"
 import React, { useEffect } from 'react';
 import { Footer } from './component/layout/Footer/Footer';
 import Home from "./component/Home/Home"
+import ProductDetails from './component/Product/ProductDetails';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
     <Router>
       <Header />
       <Route exact path="/" component={Home} />
+      <Route exact path="/product/:id" component={ProductDetails} />
       <Footer />
     </Router>
   )
